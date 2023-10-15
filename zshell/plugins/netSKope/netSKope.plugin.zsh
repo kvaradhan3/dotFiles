a6d93ffa969cb40e87c6e05b899e1e8ca6f1a863
Kannan Varadhan <kannan@ieee.org>
2023-10-15 05:18:22 -0700

    convert eskp funcs to plugins

---
+++ zshell/plugins/netSKope/netSKope.plugin.zsh
@@ -0,0 +1,33 @@
+#! /bin/zsh
+# shellcheck disable=SC2086,SC2278,SC2298,SC2296,SC2299
+# SC2086 (info): Double quote to prevent globbing and word splitting.
+# SC2296 (error): Parameter expansions can't start with (. Double check syntax.
+# SC2298 (error): ${${x}} is invalid. For expansion, use ${x}.
+#        For indirection, use arrays, ${!x} or (for sh) eval.
+# SC2278 (error): $0 can't be assigned in Ksh (but it does reflect the current function).
+# SC2299 (error): Parameter expansions can't be nested. Use temporary variables.
+#
+
+emulate -L zsh
+
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ ${zsh_loaded_plugins[-1]} != */netSkope ]] && \
+       [[ -z ${fpath[(r)${0:h}/functions]} ]]
+then
+    fpath+=( "${0:h}/functions" )
+fi
+
+netSkope_plugin_unload() {
+    for i in jf spr
+    do
+        whence -w $i &>/dev/null && unfunction $i
+    done
+
+    fpath=("${(@)fpath:#${0:A:h}}")
+    unfunction $0
+}
+
+autoload -Uz jf		# jira format definer
+autoload -Uz spr	# helper to generate github PR reference
