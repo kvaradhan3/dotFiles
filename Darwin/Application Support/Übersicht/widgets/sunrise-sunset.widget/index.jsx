8f52dce07e96b5c5231cebaf2c4dcb7e709a17a9
Kannan Varadhan <kannan@ieee.org>
2024-06-03 07:58:33 -0700

    Sunrise/Sunset, JSX version, no secrets

--- Darwin/Application Support/Übersicht/widgets/sunrise-sunset.widget/index.jsx
+++ Darwin/Application Support/Übersicht/widgets/sunrise-sunset.widget/index.jsx
@@ -1,79 +1,164 @@
-import { css } from "uebersicht";
+{/*
+  * VisualCrossing uses US ZIP codes,
+  * or "City,Country" format for location,
+  * Country is in ISO-3166-2 ALPHA-2 format
+  * (https://www.iban.com/country-codes)
+  */}
 
-/** weather codes list can eb fetched from https://weather.codes.
-    For the US, you can use the zip code as the default weather code.
-    For most countries, you can go to https://weather.codes/{{country}}
-    or check at the bottom of https://weather.codes for the list of countries.
- **/
-const location = "95129";
+import { css } from "uebersicht"
 
-/************** UI Settings **************/
-const fontColor   = "white";
-const itemPadding = "15px";
-const fontSize    = "25px";
-const fontFamily  = "Helvetica";
-const fontSizeTxt = $fontSize;
-const fontFamText = $fontFamily;
-const fontSizeSym = $fontSize;
-const fontFamSymb = "Lucida-Sans Unicode";
+const location = 95129
+const keyFile  = "$HOME/.VisualCrossingKey"
+const key      = "BAD"
+const baseURL  = new URL( "https://weather.VisualCrossing.Com/" )
+const testURL  = new URL( "http://127.0.0.1:5000/" )
+const API      = `/VisualCrossingWebServices/rest/services/timeline/${location}`
 
-export const className = `
-    bottom: 1%;
-    left: 1%;
-	z-index: 1;
-`;
+const legend = {
+    sunrise: "\u263C",
+    sunset:  "\u2600"
+};
 
-const container = css`
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-`;
+export const refreshFrequency = 1000;
 
-const unicode = css`
-  font-family: Lucida-Sans Unicode;
-  font-size:   "25px";
-  color:       ${fontColor};
-  border: 0 none;
-  padding: 5px;
-`;
-  
-const time = css`
-  font-family: "Helvetica";
-  font-size:   "25px";
-  color:       ${fontColor};
-  border: 0 none;
-  padding: 5px;
-`;
-  
-/************** UI Settings **************/
+export const initialState = {
+    status: 'INIT',
+    data: {
+        sunrise: 'INIT',
+        sunset:  'INIT'
+    }
+};
 
-export const refreshFrequency = 1000 * 15;
+{/*
+   1. read key from 1password
+   2. Use geolocation to not hardcode ${location}
+ */}
 
-export const command = "\
-  curl --silent --no-buffer https://weather.com/today/l/${location} |\
-  grep SunriseSunset |\
-  grep -E --only-matching '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))'";
+const weather = new URL(API, testURL)
+weather.searchParams.set("key", key);
+weather.searchParams.set("contentType", "json");
 
-const sunRiseSymbol = "\u263C";
-const sunRise = () => {
-  return "06:20 am";
-}
-const sunSetSymbol  = "\u2600";
-const sunSet = () => {
-  return "04:x3 pm";
-}
+export const command = (dispatch) => fetch(`${weather.href}`)
+    .then((response) => {
+        dispatch({ status: 'SUCCESS', data: response.json() })
+    })
+    .catch((error) => {
+        dispatch({ status: 'FAILURE', error: String(error) })
+    })
+
+const next = 'AB0012 '
 
 export const updateState = (event, previousState) => {
-  return event;
-};
+    switch (event.status) {
+    case 'INIT': return event.data;
+    case 'SUCCESS': return {
+        sunrise: next + String(event.data.currentConditions.sunrise),
+        sunset:  event.data.currentConditions.sunset
+    };
+    case 'FAILURE': return {
+        sunrise: "ERRORRET",
+        sunset: "<" + event.error + ">"
+    };
+    default: {
+        return {
+            sunrise: next + String(event.status),
+            sunset:  String(previousState.status)
+        };
+    }
+    }
+}
 
-export const render = ({ output }) => {
-  return (
-    <div className={container}>
-      <div className="{unicode}">{sunRiseSymbol}</div>
-      <div className="{time}">{sunRise()}</div>
-      <div className="{unicode}">{sunSetSymbol}</div>
-      <div className="{time}">{sunSet()}</div>
-    </div>
-  );
-};
+export const render = (p) => {
+    return (
+        <div className={container}>
+            <div className={symbol}>
+                <div className={sunRise}>{legend.sunrise}</div>
+            </div>
+            <div className={timeStamp}>
+                <div className={sunRise}>{p.sunrise}</div>
+            </div>
+            <div className={symbol}>
+                <div className={sunRise}>{legend.sunset}</div>
+            </div>
+            <div className={timeStamp}>
+                <div className={sunSet}>{p.sunset}</div>
+            </div>
+            <div className={debug}>LLL {String(p)}</div>
+        </div>
+    )
+}
+
+export const className = `
+  left:    1%;
+  bottom:  1%;
+  z-index: 1;
+  color: grey;
+`
+
+const container = css`
+    display:               grid;
+    grid-template-columns: auto auto auto auto;
+    border:                0 none;
+    text-align:            center;
+    vertical-align:        middle;
+`
+
+const symbol = css`
+    padding:               8px;
+    font-family:           Lucida-Sans Unicode;
+    font-size:             25px;
+`
+
+const timeStamp = css`
+    padding:               8px;
+    font-family:           Helvetica Neue;
+    font-size:             25px;
+`
+
+const sunRise = css`
+    color:                  blue;
+`
+
+const sunSet = css`
+    color:                  orange;
+`
+
+const debug = css`
+    font-family: Helvetica;
+    color: blue;
+`
+
+{/*
+   Notes:
+   - This form of command structure,
+     export const command = (dispatch) => {
+       fetch(`${weather.href}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((respdata) => {
+            return dispatch({ status: 'SUCCESS', data: respdata.curConditions });
+        })
+        .catch((error) => {
+            return dispatch({ status: 'FAILURE', error: String(error) });
+        })
+
+        }
+     runs repeatedly and throws error, "TypeError: Load failed".
+     Why?
+   - In this form:
+     export const command = (dispatch) => fetch(`${weather.href}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((respdata) => {
+        return dispatch({ status: 'SUCCESS', data: respdata.curConditions });
+      })
+      .catch((error) => {
+        return dispatch({ status: 'FAILURE', error: String(error) });
+        })
+     The event.status is undefined, event itself is an object and I cannot tell its returned fields....
+
+
+ */}
 
