acb832c1390da8c8374106e492066fc86d60b89e
Kannan Varadhan <kannan@ieee.org>
2024-06-07 11:41:38 -0700

    magit turn off line numbers for hunk staging

--- common/emacs/45git.el
+++ common/emacs/45git.el
@@ -3,7 +3,12 @@
 (require 'magit)
 
 (use-package magit-delta
-  :hook (magit-mode . magit-delta-mode))
+  :hook (magit-mode . magit-delta-mode)
+  :config
+  (setq magit-delta-delta-args (append magit-delta-delta-args
+                                       '("--features"
+                                         "magit-delta")))
+  )
 
 (use-package forge
   :after magit
